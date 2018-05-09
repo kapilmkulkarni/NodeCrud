287f5eb6b72a24e81b725868b39f7b587f9f6e0e
Ashish <Ashish@espl.local>
2018-05-09 16:24:25 +0530

    neww url

--- server/config/auth.js
+++ server/config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 	'facebookAuth': {
 		'clientID': '1894624250587693',
 		'clientSecret': '297aa884b33ae57a40978969b80b9d4f',
-		'callbackURL': 'https://agile-citadel-47885.herokuapp.com/auth/facebook/callback'
+		'callbackURL': 'https://limitless-fjord-36017.herokuapp.com/auth/facebook/callback'
 	},
 
 	'googleAuth': {
